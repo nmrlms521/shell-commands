a0828a7620f9c5fbf638e3056c565d1008354ecd
mohan <nerjimohanarao@gmail.com>
2025-11-20 13:01:07 +0530

    added echo color

--- colors.sh
+++ colors.sh
@@ -7,3 +7,12 @@ echo -e "\e[32mOrder Placed successfully\e[0m"
 echo -e "\e[31mFailed\e[0m"
 echo -e "\e[33mProcessing\e[0m"
 echo -e "\e[34murl link\e[0m"
+
+RED="\e[91m"
+GREEN="\e[92m"
+YELLOW="\e[93m"
+BLUE="\e[94m"
+RESET="\e[0m"
+
+echo -e "${GREEN}Task Completed Successfully!${RESET}"
+echo -e "${RED}Error occurred!${RESET}"
