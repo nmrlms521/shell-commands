c281768ecf456f19e6609cd7642394bd781fd774
mohan <nerjimohanarao@gmail.com>
2025-11-20 13:34:42 +0530

    var intro

--- variables.sh
+++ variables.sh
@@ -1,3 +1,3 @@
-echo -e "\e[91m\e[1mVaraibles\e[0m"
-
+echo -e "\e[93m================================\e[0m"
 echo -e "\e[91m\e[1m\e[4mVaraibles\e[0m"
+echo -e "\e[93m================================\e[0m"
