6d08ad0d849d3c9aa5546483eae64c6a0ca3bfcb
mohan <nerjimohanarao@gmail.com>
2025-11-20 12:58:27 +0530

    added echo color

--- colors.sh
+++ colors.sh
@@ -1,4 +1,9 @@
-echo -e \e[32mOrder Placed successfully
-echo -e \e[31mFailed
-echo -e \e[33mProcessing
-echo -e \e[34murl link
+echo -e \\e[32mOrder Placed successfully
+echo -e \\e[31mFailed
+echo -e \\e[33mProcessing
+echo -e \\e[34murl link
+
+echo -e "\e[32mOrder Placed successfully\e[0m"
+echo -e "\e[31mFailed\e[0m"
+echo -e "\e[33mProcessing\e[0m"
+echo -e "\e[34murl link\e[0m"
